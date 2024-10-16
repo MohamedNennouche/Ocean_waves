0a0c029ce75fcace77ad28efe76a49a983631441
Moh <moohaameed.nennouche@gmail.com>
2024-10-16 17:10:32 +0200

    Préparation de fichiers à remplir par la suite

---
+++ OceanWaves/irradiance.py
@@ -0,0 +1,2 @@
+# submodule to calculate the radiometry parameters of a generated surface
+# coming soon
---
+++ OceanWaves/underwater_acoustics.py
@@ -0,0 +1,2 @@
+# submodule to calculate the impact of the water surface on the propagation of acoustic waves
+## coming soon
