892ec9010ac14fa3389442b611374049576db500
Moh <moohaameed.nennouche@gmail.com>
2024-10-16 15:54:06 +0200

    Mise en place des fonctions pour générer les spreading functions

---
+++ OceanWaves/spreading_functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def ittc(thetas:np.array) : 
+    return (2/np.pi)*np.cos(thetas)**2
+
+def issc(thetas:np.array) :
+    return (8/(3*np.pi))*np.cos(thetas)**4
+
+def swop(thetas:np.array, omega:float, U:float, g:float=9.81) :
+    p = 0.5 + 0.82*np.exp(-0.5*((omega*U)/g)**4)
+    q = 0.32*np.exp(-0.5*((omega*U)/g)**4)
+    
+    D = (1/np.pi)*(1 + p*np.cos(2*thetas) + q*np.cos(4*thetas))
+    
+    return D
